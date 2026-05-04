ddfdf5b3b43d6f947f690523273b9fd6e45e2f1f
ranjitkal <cnranjit@gmail.com>
2026-05-04 08:48:48 +0000

    updated code3

--- tests/loginexample.spec.ts
+++ tests/loginexample.spec.ts
@@ -1,31 +1,59 @@
-
-
-
-import { test , expect, Locator } from "@playwright/test"
-
-
-test ("practice for locators",async({ page })=>{
-
-
-    //for getvisable text
-    await page.goto("https://demo.nopcommerce.com/");
+import { test, Browser, Page, BrowserContext } from '@playwright/test';
+import { BrowserFactory } from './BrowserFactory';
+ 
+let browser: Browser;
+let context: BrowserContext;
+let page: Page;
+ 
+test.describe.configure({ mode: 'serial' }); // ✅ serial execution
+ 
+test.describe('@smoke Browser Factory Example', () => {
+ 
+  test.beforeAll(async () => {
+    const browserName = process.env.BROWSER || 'chromium';
+   
+    browser = await BrowserFactory.getBrowser(browserName);
+ 
+    context = await browser.newContext();
+    page = await context.newPage();
+  });
+ 
+  test('Open application', async () => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  });
+ 
+  test('Validate title', async () => {
     await page.waitForTimeout(2000);
-    const logo:Locator=page.getByAltText("nopCommerce demo store");
-    await page.waitForTimeout(2000);
-//    logo.click();
-    await expect(logo).toBeVisible();
-    await expect(page.getByText("Welcome to our store")).toBeVisible();
+    console.log(await page.title());
+    const Title = await page.title();
+      await page.waitForTimeout(2000);
+    console.log("Page title is: " + Title);
+  //await expect(page).toHaveTitle(/Box | Login/);
+
+
+ await page.fill('input[name="username"]','Admin');
+
+ await page.locator('input[name="password"]').pressSequentially('admin123',{delay:300});
+   await page.waitForTimeout(2000);
+ //await page.fill('input[name="password"]','Ranjit@7001');
+ await page.waitForTimeout(2000);
+await page.locator('//button[@type="submit"]').click({button:'left' , delay:3000});
+ 
+await page.waitForTimeout(2000);
+await page.locator('//*[@class="oxd-userdropdown-tab"]').click({button:'left' , delay:3000});
+await page.locator('//*[text()="Logout"]').click({button:'left' , delay:3000});
 
 
 
 
-})
+  });
 
 
 
-
-
-
-
-
-
+ 
+  test.afterAll(async () => {
+  await browser.close();
+   
+  });
+ 
+});
