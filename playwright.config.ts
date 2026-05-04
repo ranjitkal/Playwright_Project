81f30917cfc36f8fdc13516bc18fdc99ea95be5a
ranjitkal <cnranjit@gmail.com>
2026-05-04 09:23:56 +0000

    CI-CD-Configfile

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,21 +34,15 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+  {
+    name: 'chromium',
+    use: { browserName: 'chromium' }
+  },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
 
+
+    
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
