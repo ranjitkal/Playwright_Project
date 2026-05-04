c18e6a7b182f0169c4cd1ea081a1169020ddeaef
ranjitkal <cnranjit@gmail.com>
2026-05-04 09:12:31 +0000

    Updated_code5

--- tests/BrowserFactory.ts
+++ tests/BrowserFactory.ts
@@ -1,39 +1,33 @@
 
 
-import { chromium, firefox,webkit,Browser  } from "@playwright/test";
+import { chromium, firefox, webkit, Browser } from "@playwright/test";
 
-export class BrowserFactory
-{
-public static async getBrowser(browserName: string): Promise<Browser>
-{
-switch(browserName.toLowerCase())
-{
+export class BrowserFactory {
+  public static async getBrowser(browserName: string): Promise<Browser> {
+    switch (browserName.toLowerCase()) {
 
-    case 'chromium':
-   return await chromium.launch({
-    headless: false,
-   });
-    
+      case "chromium":
+        return await chromium.launch({
+          headless: true,
+          args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        });
 
-   
-    case 'firefox':
-   return await chromium.launch({
-    headless: false,
-   });
-    
+      case "firefox":
+        return await firefox.launch({
+          headless: true,
+          args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        });
 
-   
-    case 'webkit':
-   return await chromium.launch({
-    headless: false,
-   });
-    
+      case "webkit":
+        return await webkit.launch({
+          headless: true,
+          args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        });
 
-    default:
-        throw new Error ('Invalid browser name : ${browserName}. use chrome , firefox , or webkit ');
+      default:
+        throw new Error(
+          `Invalid browser name: ${browserName}. Use chromium, firefox, or webkit`
+        );
+    }
+  }
 }
-
-}
-
-}
-
